2bc2a0c718e4e8fdd01edd66a440579be30961b4
perebartrolisimo <pere.bartroli@ub.uu.se>
2019-10-09 12:11:26 +0000

    CORA-696 Added more specific messagaeRoutingInfo for JMS and AMQP

---
+++ src/main/java/se/uu/ub/cora/messaging/AmqpMessageRoutingInfo.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.uu.ub.cora.messaging;
+
+public class AmqpMessageRoutingInfo extends MessageRoutingInfo {
+
+	public final String virtualHost;
+	public final String exchange;
+
+	public AmqpMessageRoutingInfo(String hostname, String port, String routingKey,
+			String virtualHost, String exchange) {
+		super(hostname, port, routingKey);
+		this.virtualHost = virtualHost;
+		this.exchange = exchange;
+	}
+
+}
